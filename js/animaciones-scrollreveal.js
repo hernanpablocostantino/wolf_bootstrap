7b427d511e3646b5059a3a833d49ae77f3073ce4
hernanpablocostantino <admhernancostantino@gmail.com>
2022-11-22 17:26:43 -0300

    commit final

---
+++ js/animaciones-scrollreveal.js
@@ -0,0 +1,42 @@
+window.sr = ScrollReveal();
+ScrollReveal().reveal(".navbar", {
+  reset: true,
+  duration: 2000,
+  origin: "bottom"
+});
+ScrollReveal().reveal(".header_left", {
+  reset: true,
+  duration: 2000,
+  origin: "top",
+  distance: "300px"
+});
+ScrollReveal().reveal(".header_right", {
+  reset: true,
+  duration: 2000,
+  origin: "right",
+  distance: "300px"
+});
+ScrollReveal().reveal(".btn_comunicado", {
+  reset: true,
+  duration: 2000,
+  origin: "bottom",
+  delay: 2000
+});
+ScrollReveal().reveal(".title", {
+  reset: true,
+  duration: 2000,
+  origin: "left",
+  delay: 900
+});
+ScrollReveal().reveal(".txt", {
+  reset: true,
+  duration: 1800,
+  delay: 1000,
+  origin: "bottom"
+});
+ScrollReveal().reveal(".img", {
+  reset: true,
+  duration:1800,
+  delay: 1000,
+  origin: "top"
+});  
